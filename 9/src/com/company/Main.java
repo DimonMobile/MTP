4291ae8750dd8f73903ef47b8d1aeb2f5696b317
Dimon Mobile <mobik.dimka@gmail.com>
2019-04-19 21:29:23 +0300

    Part 2 first task complete

--- 9/src/com/company/Main.java
+++ 9/src/com/company/Main.java
@@ -4,6 +4,12 @@ import io.reactivex.*;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.subjects.BehaviorSubject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -28,6 +34,24 @@ public class Main {
 ////        a.onNext(3);
 ////        a.onNext(4);
 
+//  === PART 2
+        try {
+            URL url = new URL("https://example.com");
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            System.out.println(connection.getResponseCode());
+            System.out.println(connection.getResponseMessage());
 
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer response = new StringBuffer();
+            String inputLine;
+            while((inputLine = reader.readLine()) != null)
+                response.append(inputLine).append("\n");
+            reader.close();
+
+            System.out.println(response);
+        } catch(IOException e) {
+            System.out.println(e.getStackTrace().toString());
+        }
     }
 }
