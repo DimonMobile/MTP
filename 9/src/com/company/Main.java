5ee079b3db75c789077043544547ce312c72b11c
Dimon Mobile <mobik.dimka@gmail.com>
2019-04-19 21:15:34 +0300

    Part1 for 9 lab complete

---
+++ 9/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import io.reactivex.*;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.BehaviorSubject;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        //  === PART 1
+//        BehaviorSubject<Integer> a = BehaviorSubject.create();
+//        BehaviorSubject<Integer> b = BehaviorSubject.create();
+//
+////        //  *** Uncomment for first task
+////        {
+////            Observable<Integer> c = Observable.combineLatest(a, b, (i1, i2) -> i1 + i2);
+////            c.subscribe(System.out::println);
+////            a.onNext(10);
+////            b.onNext(10);
+////            a.onNext(20);
+////        }
+//
+////        //  *** Uncomment for second task
+////        Observable<Integer> c = a.filter(i -> i % 2 == 0).map(i -> i * i);
+////        c.subscribe(System.out::println);
+////        a.onNext(1);
+////        a.onNext(2);
+////        a.onNext(3);
+////        a.onNext(4);
+
+
+    }
+}
