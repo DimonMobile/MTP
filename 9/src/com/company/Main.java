7be5ebe0d3a091efa662b6d9f802d1c6159864f6
Dimon Mobile <mobik.dimka@gmail.com>
2019-04-19 21:37:54 +0300

    Parts replaced in separated methods

--- 9/src/com/company/Main.java
+++ 9/src/com/company/Main.java
@@ -12,46 +12,56 @@ import java.net.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-//        //  === PART 1
-//        BehaviorSubject<Integer> a = BehaviorSubject.create();
-//        BehaviorSubject<Integer> b = BehaviorSubject.create();
-//
-////        //  *** Uncomment for first task
-////        {
-////            Observable<Integer> c = Observable.combineLatest(a, b, (i1, i2) -> i1 + i2);
-////            c.subscribe(System.out::println);
-////            a.onNext(10);
-////            b.onNext(10);
-////            a.onNext(20);
-////        }
-//
-////        //  *** Uncomment for second task
-////        Observable<Integer> c = a.filter(i -> i % 2 == 0).map(i -> i * i);
-////        c.subscribe(System.out::println);
-////        a.onNext(1);
-////        a.onNext(2);
-////        a.onNext(3);
-////        a.onNext(4);
-
-//  === PART 2
-        try {
-            URL url = new URL("https://example.com");
-            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            connection.setRequestMethod("GET");
-            System.out.println(connection.getResponseCode());
-            System.out.println(connection.getResponseMessage());
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuffer response = new StringBuffer();
-            String inputLine;
-            while((inputLine = reader.readLine()) != null)
-                response.append(inputLine).append("\n");
-            reader.close();
-
-            System.out.println(response);
-        } catch(IOException e) {
-            System.out.println(e.getStackTrace().toString());
+    private static void partOne(int index) {
+        BehaviorSubject<Integer> a = BehaviorSubject.create();
+        BehaviorSubject<Integer> b = BehaviorSubject.create();
+        if (index == 1)
+        {
+            Observable<Integer> c = Observable.combineLatest(a, b, (i1, i2) -> i1 + i2);
+            c.subscribe(System.out::println);
+            a.onNext(10);
+            b.onNext(10);
+            a.onNext(20);
         }
+        else if (index == 2) {
+            Observable<Integer> c = a.filter(i -> i % 2 == 0).map(i -> i * i);
+            c.subscribe(System.out::println);
+            a.onNext(1);
+            a.onNext(2);
+            a.onNext(3);
+            a.onNext(4);
+        }
+    }
+
+    private static void partTwo(int index) {
+        if (index == 1) {
+            try {
+                URL url = new URL("https://example.com");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                System.out.println(connection.getResponseCode());
+                System.out.println(connection.getResponseMessage());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuffer response = new StringBuffer();
+                String inputLine;
+                while ((inputLine = reader.readLine()) != null)
+                    response.append(inputLine).append("\n");
+                reader.close();
+
+                System.out.println(response);
+            } catch (IOException e) {
+                System.out.println(e.getStackTrace().toString());
+            }
+        } else if (index == 2) {
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+//        partOne(1);
+        partTwo(2);
+
     }
 }
