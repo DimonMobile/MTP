fd8dfadaf760731c0cbaeaa706637c0ea8c0469f
Dimon Mobile <mobik.dimka@gmail.com>
2019-04-20 12:53:15 +0300

    Lab10 servlet tested; closes #9

---
+++ 10/src/ServletTest.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello World!");
+    }
+}
