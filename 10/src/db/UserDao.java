96ce914fa99ef67abb63546201161db9413fbefb
Dimon Mobile <mobik.dimka@gmail.com>
2019-04-20 14:30:15 +0300

    DAO updated

--- 10/src/db/UserDao.java
+++ 10/src/db/UserDao.java
@@ -1,5 +1,22 @@
 package db;
 
-public class UserDao {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class UserDao {
+    public UserDao() throws ClassNotFoundException, SQLException {
+        Statement statement = DatabaseConnection.getStatement();
+        ResultSet resultSet = statement.executeQuery("SHOW TABLES");
+        boolean exists = false;
+        while(resultSet.next()) {
+            if (resultSet.getString(1).equals("users")) {
+                exists = true;
+                break;
+            }
+        }
+        if (!exists) {
+            statement.execute("CREATE TABLE users(email VARCHAR(50), password VARCHAR(256) NOT NULL, CONSTRAINT users_pk PRIMARY KEY users(email)");
+        }
+    }
 }
