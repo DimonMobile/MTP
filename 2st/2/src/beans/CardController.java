74d73a2bf1c9b5f128fc3f9fae972385f6a7733d
Dimon Mobile <mobik.dimka@gmail.com>
2019-09-12 17:38:58 +0300

    #23: getters and setters for bean implemented

--- 2st/2/src/beans/CardController.java
+++ 2st/2/src/beans/CardController.java
@@ -8,6 +8,47 @@ import java.util.Date;
 @RequestScoped
 public class CardController {
     private int cardId;
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(int cardId) {
+        this.cardId = cardId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getCvc() {
+        return cvc;
+    }
+
+    public void setCvc(int cvc) {
+        this.cvc = cvc;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
     private String name;
     private Date dueDate;
     private int cvc;
